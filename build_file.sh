f725592ed3e53befd56d68667bb18e9f072bc030
MorenoDev <moreno.deveng@gmail.com>
2026-02-02 13:21:25 -0400

    sIN BUILD

--- build_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "--- 1. Instalando dependencias de Python ---"
-# Usamos --break-system-packages para permitir la instalación en el entorno de Vercel
-python3.12 -m pip install -r requirements.txt --break-system-packages
-
-echo "--- ¡PROCESO FINALIZADO EXITOSAMENTE! ---"
