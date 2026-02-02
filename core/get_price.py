3a58d9566bdfc350eb06469ceade59563858b838
MorenoDev <moreno.deveng@gmail.com>
2026-02-02 16:25:23 -0400

    ajust de la ruta ssl

--- core/get_price.py
+++ core/get_price.py
@@ -35,7 +35,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
 
-ssl_contenido = ssl.create_default_context(cafile=CERT_PATH)
+ssl_contenido = ssl.create_default_context(cafile=CURRENT_DIR / 'bcvcert.crt')
 
 async def obtener_valor_usdt():
     """Busca el dato directamente en el endpoint de Binance"""
