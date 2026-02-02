45ab0aa92c0a658a3f95d599cd2efc1a459bc884
MorenoDev <moreno.deveng@gmail.com>
2026-02-02 14:33:39 -0400

    build

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 echo "🔥 [FORCE BUILD] Borrando rastros e instalando desde cero..."
 
-# Forzamos a que use el pip de la versión de Python actual
-python3 -m pip install --upgrade pip
-
-# --no-cache-dir: No uses nada guardado
-# -vv: Super verboso (verás cada link de descarga)
-python3 -m pip install --no-cache-dir --force-reinstall -vv -r requirements.txt
+python3.12 -m pip install -r requirements.txt --break-system-packages
 
 echo "📊 Verificación de paquetes instalados:"
 pip list
