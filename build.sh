a7af97acbca152d5b49746d5091ec77f9de322a7
MorenoDev <moreno.deveng@gmail.com>
2026-02-02 14:59:30 -0400

    modificando a app

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 echo "🔥 [FORCE INSTALL] Eliminando cache y forzando descarga..."
 
-# Forzamos a que pip sea extremadamente ruidoso (-vv) 
-# y que no use nada guardado en cache (--no-cache-dir)
-python3.12 -m pip install --upgrade pip
 python3.12 -m pip install --no-cache-dir --force-reinstall -vv -r requirements.txt
 
 echo "📋 Verificando si dotenv está presente:"
